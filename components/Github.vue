6454dbd4e9b4f1849fd50989d9e2e1aa626d2f4d
Akira Shinohara <akira.shinohara@mojamoja.cloud>
2020-07-20 19:49:11 +0900

    feat: create Github icon component

---
+++ components/Github.vue
@@ -0,0 +1,17 @@
+<template>
+  <a :href="github_uri" class="text-dark" target="_blank">
+    <fa :icon="['fab', 'github']" class="fa-4x" />
+  </a>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return {
+      github_uri: 'https://github.com/s10akir',
+    }
+  },
+})
+</script>
