e11f8727c84be32c58c2b347572e61c16ce929cf
Akira Shinohara <akira.shinohara@mojamoja.cloud>
2020-10-16 12:52:05 +0000

    feat: support pwa

--- nuxt.config.js
+++ nuxt.config.js
@@ -48,6 +48,7 @@ export default {
     '@nuxt/typescript-build',
     // Doc: https://github.com/nuxt-community/stylelint-module
     '@nuxtjs/stylelint-module',
+    '@nuxtjs/pwa',
   ],
   /*
    ** Nuxt.js modules
