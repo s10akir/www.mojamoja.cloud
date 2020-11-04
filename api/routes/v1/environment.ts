61f2315690c1086eabe8f64162cbb99a75681f6e
Akira Shinohara <akira.shinohara@mojamoja.cloud>
2020-11-04 22:38:22 +0900

    feat: create GET v1/environment/fetch api

--- api/routes/v1/environment.ts
+++ api/routes/v1/environment.ts
@@ -1,6 +1,9 @@
+import axios from 'axios'
+
 import { Router } from 'express'
 import { RemoLog } from '../../entity/RemoLog'
 
+const NATURE_TOKEN = process.env.NATURE_TOKEN || ''
 const router = Router()
 
 router.get('/latest', async (_, res) => {
@@ -12,8 +15,36 @@ router.get('/latest', async (_, res) => {
     brightness: remoLog?.brightness ?? null,
     motion: remoLog?.motion ?? null,
     created_at: remoLog?.createdAt ?? null,
-    updated: remoLog?.updatedAt ?? null
+    updated: remoLog?.updatedAt ?? null,
   })
+})
+
+router.get('/fetch', async (_, res) => {
+  axios
+    .get('https://api.nature.global/1/devices', {
+      headers: {
+        Authorization: `Bearer ${NATURE_TOKEN}`,
+      },
+    })
+    .then((result) => {
+      const status = result.status
+      if (status === 200) {
+        const remoLog = new RemoLog()
+
+        remoLog.temperature = result.data[0].newest_events.te.val
+        remoLog.humidity = result.data[0].newest_events.hu.val
+        remoLog.brightness = result.data[0].newest_events.il.val
+        remoLog.motion = result.data[0].newest_events.mo.val
+
+        remoLog.save()
+
+        res.status(status)
+        res.send(status)
+      }
+    }).catch(err => {
+      res.status(503)
+      res.json({err})
+    })
 })
 
 export default router
