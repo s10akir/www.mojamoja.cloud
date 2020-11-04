91e2a21b51978d15880a16562868bc6c1df79c07
Akira Shinohara <akira.shinohara@mojamoja.cloud>
2020-11-04 22:17:33 +0900

    fix: typeorm connection pool issue when nuxt dev HMR

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,25 @@
 import 'reflect-metadata'
 
 import express from 'express'
-import { createConnection } from 'typeorm'
+import { createConnection, getConnection } from 'typeorm'
 
 import router from './routes'
 
-// TODO: error handling
-createConnection()
+// FIXME: 開発環境でのNuxtのHMR時にDBへのコネクションプールが維持されたまま再起動するので、
+//        getConnectionのConnectionNotFoundErrorを利用してコネクションプール初期化を行っている
+//        普通に最悪
+try {
+  const connection = getConnection()
+
+  ;(async () => {
+    // promiseなので同期を取らないと次のコネクションプール初期化に失敗する
+    await connection.close().then(() => createConnection())
+  })()
+} catch {
+  // catchにあるがこれが期待する正常時動作
+  // （アプリケーション起動時はコネクションプールがないことを期待する）
+  createConnection()
+}
 
 const app = express()
 
